bff0c872ee22b1d7a40b37f4cbe23fb0ddfb012e
JanWosnitza <jan_wosnitza@hotmail.com>
2016-04-06 20:24:47 +0200

    replace recursive loop

--- Compiler.fs
+++ Compiler.fs
@@ -1,34 +1,35 @@
 ﻿module BrainFark.Compiler
 
 let fromString (code:string) =        
-    let rec loopIfNotZero body memory =
+    let rec loop body memory =
         match Memory.get memory with
         | 0uy -> memory
-        | _ -> memory |> body |> loopIfNotZero body
+        | _ -> memory |> body |> loop body
 
-    let rec loop fs f =
-        function
-        | '>'::xs -> loop fs (f >> Memory.moveRight) xs
-        | '<'::xs -> loop fs (f >> Memory.moveLeft) xs
+    let f, fs =
+        code
+        |> Seq.fold
+            (fun (f,fs) x ->
+            match x with
+            | '>' -> (f >> Memory.moveRight, fs)
+            | '<' -> (f >> Memory.moveLeft, fs)
 
-        | '+'::xs -> loop fs (f >> Memory.increment) xs
-        | '-'::xs -> loop fs (f >> Memory.decrement) xs
+            | '+' -> (f >> Memory.increment, fs)
+            | '-' -> (f >> Memory.decrement, fs)
 
-        | '.'::xs -> loop fs (f >> Memory.write) xs
-        | ','::xs -> loop fs (f >> Memory.read) xs
+            | '.' -> (f >> Memory.write, fs)
+            | ',' -> (f >> Memory.read, fs)
 
-        | '['::xs -> loop (f::fs) id xs
-        | ']'::xs ->
-            match fs with
-            | f'::fs' -> loop fs' (f' >> loopIfNotZero f) xs
-            | [] -> failwith "unmatched ]"
+            | '[' -> (id, f::fs)
+            | ']' ->
+                match fs with
+                | f'::fs' -> (f' >> loop f, fs')
+                | [] -> failwith "unmatched ]"
 
-        | _::xs -> loop fs f xs
-        | [] ->
-            match fs with
-            | [] -> f
-            | _ -> failwith "unmatched ["
+            | _ -> (f,fs)
+            )
+            (id,[])
 
-    code
-    |> Seq.toList
-    |> loop [] id
+    match fs with
+    | [] -> f
+    | _ -> failwith "unmatched ["
