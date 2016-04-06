951be9bae078b98f69e406e0ca650481018c1f86
JanWosnitza <jan_wosnitza@hotmail.com>
2016-04-06 19:04:16 +0200

    unify api

--- Compiler.fs
+++ Compiler.fs
@@ -1,24 +1,28 @@
 ﻿module BrainFark.Compiler
 
-let rec private loopIfNotZero body memory =
-    match Memory.get memory with
-    | 0uy -> memory
-    | _ -> memory |> body |> loopIfNotZero body
-
 let fromString (code:string) =        
+    let rec loopIfNotZero body memory =
+        match Memory.get memory with
+        | 0uy -> memory
+        | _ -> memory |> body |> loopIfNotZero body
+
     let rec loop fs f =
         function
         | '>'::xs -> loop fs (f >> Memory.moveRight) xs
         | '<'::xs -> loop fs (f >> Memory.moveLeft) xs
+
         | '+'::xs -> loop fs (f >> Memory.increment) xs
         | '-'::xs -> loop fs (f >> Memory.decrement) xs
+
         | '.'::xs -> loop fs (f >> Memory.write) xs
         | ','::xs -> loop fs (f >> Memory.read) xs
+
         | '['::xs -> loop (f::fs) id xs
         | ']'::xs ->
             match fs with
             | f'::fs' -> loop fs' (f' >> loopIfNotZero f) xs
             | [] -> failwith "unmatched ["
+
         | _::xs -> loop fs f xs
         | [] ->
             match fs with
