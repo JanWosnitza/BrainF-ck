349868146e25ceefd5a8b62de7579d2d0dfba4af
JanWosnitza <jan_wosnitza@hotmail.com>
2016-04-06 17:09:18 +0200

    flattened match with

--- Interpreter.fs
+++ Interpreter.fs
@@ -16,49 +16,47 @@ let run code =
             | _ -> skip count cs
         | [] -> failwith "unmatched ["
 
-    let rec loop (left,current,right) stack code =
-        match code with
-        | c::cs ->
-            match c with
-            | '>' -> 
-                match right with
-                | [] -> loop (current::left, 0uy, []) stack cs
-                | r::rs -> loop (current::left, r, rs) stack cs
+    let rec loop (left,current,right) stack =
+        function
+        | '>'::cs -> 
+            match right with
+            | [] -> loop (current::left, 0uy, []) stack cs
+            | r::rs -> loop (current::left, r, rs) stack cs
 
-            | '<' ->
-                match left with
-                | [] -> loop ([], 0uy, current::right) stack cs
-                | l::ls -> loop (ls, l, current::right) stack cs
+        | '<'::cs ->
+            match left with
+            | [] -> loop ([], 0uy, current::right) stack cs
+            | l::ls -> loop (ls, l, current::right) stack cs
 
-            | '+' ->
-                loop (left, current + 1uy, right) stack cs
+        | '+'::cs ->
+            loop (left, current + 1uy, right) stack cs
 
-            | '-' ->
-                loop (left, current - 1uy, right) stack cs
+        | '-'::cs ->
+            loop (left, current - 1uy, right) stack cs
 
-            | '.' ->
-                Console.Write( Encoding.ASCII.GetString( [|current|] ) )
-                loop (left, current, right) stack cs
+        | '.'::cs ->
+            Console.Write( Encoding.ASCII.GetString( [|current|] ) )
+            loop (left, current, right) stack cs
 
-            | ',' ->
-                let i = Console.Read()
-                if i < -1 then failwith "aborted"
-                else loop (left, byte i, right) stack cs
+        | ','::cs ->
+            let i = Console.Read()
+            if i < -1 then failwith "aborted"
+            else loop (left, byte i, right) stack cs
 
-            | '[' ->
+        | '['::cs ->
+            match current with
+            | 0uy -> loop (left, current, right) stack (skip 1 cs)
+            | _ -> loop (left, current, right) (cs::stack) cs
+
+        | ']'::cs ->
+            match stack with
+            | s::ss ->
                 match current with
-                | 0uy -> loop (left, current, right) stack (skip 1 cs)
-                | _ -> loop (left, current, right) (cs::stack) cs
+                | 0uy -> loop (left, current, right) ss cs
+                | _ -> loop (left, current, right) stack s
+            | [] -> failwith "unmatched ["
 
-            | ']' ->
-                match stack with
-                | s::ss ->
-                    match current with
-                    | 0uy -> loop (left, current, right) ss cs
-                    | _ -> loop (left, current, right) stack s
-                | [] -> failwith "unmatched ["
-
-            | _ -> loop (left, current, right) stack cs
+        | _::cs -> loop (left, current, right) stack cs
         | [] ->
             match stack with
             | s::ss -> failwith "unmatched ["
